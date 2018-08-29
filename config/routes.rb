f32054f8dbaa344076fbf65e1d7fa330dfb45ff1
Linda <oneill.linda21@gmail.com>
2018-08-29 10:05:51 -0500

    add navbar and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'homepage#home'
+  get 'contact' => 'contact#show'
 
+  get 'work' => 'work#index'
+
+  get 'about' => 'homepage#about'
+
+  root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
