59b37838d7310a94f6ec14104ee3184e87d9d116
Linda <oneill.linda21@gmail.com>
2018-09-10 17:59:57 -0500

    add homepage nav

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+
+  $(".home-menu-link").click(function(e) {
+    e.preventDefault();
+    $(".home-menu-overlay").toggleClass("open");
+    $(".home-menu").toggleClass("open");
+  });
+
+});
